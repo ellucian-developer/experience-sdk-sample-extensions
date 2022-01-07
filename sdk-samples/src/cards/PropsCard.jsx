2037f6852b10a84acda7fd078ce641bc292bba32
Tyler Nadolski <tyler.nadolski@ellucian.com>
2022-01-07 11:11:37 -0500

    Add hooks example

--- sdk-samples/src/cards/PropsCard.jsx
+++ sdk-samples/src/cards/PropsCard.jsx
@@ -1,8 +1,22 @@
 import { withStyles } from '@ellucian/react-design-system/core/styles';
 import { spacing40 } from '@ellucian/react-design-system/core/styles/tokens';
+import { Typography, TextLink } from '@ellucian/react-design-system/core';
 import PropTypes from 'prop-types';
 import React from 'react';
-import { withIntl } from '../utils/ReactIntlProviderWrapper';
+import {
+    useCache,
+    useCardInfo,
+    useData,
+    useExperienceInfo,
+    useExtensionControl,
+    useExtensionInfo,
+    useThemeInfo,
+    useUserInfo,
+    useDashboardInfo,
+    useCardControl,
+    usePageControl,
+    usePageInfo
+} from '@ellucian/experience-extension/extension-utilities'
 
 const styles = () => ({
     card: {
@@ -16,11 +30,40 @@ const styles = () => ({
 const PropsCard = (props) => {
     const { classes } = props;
 
-    return <pre className={classes.card}>{JSON.stringify(props, undefined, 2)}</pre>;
+    return (
+        <div className={classes.card}>
+            <Typography variant={'h2'}>
+                Properties
+            </Typography>
+            <pre className={classes.card}>{JSON.stringify(props, undefined, 3)}</pre>
+            <Typography variant={'h2'}>
+                Hooks
+            </Typography>
+            <pre className={classes.card}> useCache {JSON.stringify(useCache(), undefined, 3)}</pre>
+            <pre className={classes.card}> useCardInfo {JSON.stringify(useCardInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useData {JSON.stringify(useData(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExperienceInfo {JSON.stringify(useExperienceInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExtensionControl {JSON.stringify(useExtensionControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExtensionInfo {JSON.stringify(useExtensionInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useThemeInfo {JSON.stringify(useThemeInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useUserInfo {JSON.stringify(useUserInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useDashboardInfo {JSON.stringify(useDashboardInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useCardControl {JSON.stringify(useCardControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> usePageControl {JSON.stringify(usePageControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> usePageInfo {JSON.stringify(usePageInfo(), undefined, 3)}</pre>
+            <Typography>
+                For more information regarding hooks and props, visit the
+                <TextLink href="https://resources.elluciancloud.com/bundle/ellucian_experience_acn_use/page/c_props_hooks_sdk.html" target="_blank">
+                    Props and Hooks
+                </TextLink>
+                section of the Ellucian Experience SDK documentation.
+            </Typography>
+        </div>
+    );
 };
 
 PropsCard.propTypes = {
     classes: PropTypes.object.isRequired
 };
 
-export default withIntl(withStyles(styles)(PropsCard));
+export default withStyles(styles)(PropsCard);
--- sdk-samples/src/page/index.jsx
+++ sdk-samples/src/page/index.jsx
@@ -1,18 +1,65 @@
 import { withStyles } from '@ellucian/react-design-system/core/styles';
-import { spacingSmall } from '@ellucian/react-design-system/core/styles/tokens';
+import { spacing20 } from '@ellucian/react-design-system/core/styles/tokens';
+import { Typography, TextLink } from '@ellucian/react-design-system/core';
 import PropTypes from 'prop-types';
 import React from 'react';
+import {
+    useCache,
+    useCardInfo,
+    useData,
+    useExperienceInfo,
+    useExtensionControl,
+    useExtensionInfo,
+    useThemeInfo,
+    useUserInfo,
+    useDashboardInfo,
+    useCardControl,
+    usePageControl,
+    usePageInfo
+} from '@ellucian/experience-extension/extension-utilities'
 
 const styles = () => ({
     card: {
-        margin: `0 ${spacingSmall}`
+        margin: `0 ${spacing20}`
     }
 });
 
 const PropsPage = (props) => {
     const { classes } = props;
+    const { setPageTitle } = usePageControl();
 
-    return <pre className={classes.card}>{JSON.stringify(props, undefined, 2)}</pre>;
+    setPageTitle("Props and Hooks");
+
+    return (
+        <div className={classes.card}>
+            <Typography variant={'h2'}>
+                Properties
+            </Typography>
+            <pre className={classes.card}>{JSON.stringify(props, undefined, 3)}</pre>
+            <Typography variant={'h2'}>
+                Hooks
+            </Typography>
+            <pre className={classes.card}> useCache {JSON.stringify(useCache(), undefined, 3)}</pre>
+            <pre className={classes.card}> useCardInfo {JSON.stringify(useCardInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useData {JSON.stringify(useData(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExperienceInfo {JSON.stringify(useExperienceInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExtensionControl {JSON.stringify(useExtensionControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> useExtensionInfo {JSON.stringify(useExtensionInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useThemeInfo {JSON.stringify(useThemeInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useUserInfo {JSON.stringify(useUserInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useDashboardInfo {JSON.stringify(useDashboardInfo(), undefined, 3)}</pre>
+            <pre className={classes.card}> useCardControl {JSON.stringify(useCardControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> usePageControl {JSON.stringify(usePageControl(), undefined, 3)}</pre>
+            <pre className={classes.card}> usePageInfo {JSON.stringify(usePageInfo(), undefined, 3)}</pre>
+            <Typography>
+                For more information regarding hooks and props, visit the
+                <TextLink href="https://resources.elluciancloud.com/bundle/ellucian_experience_acn_use/page/c_props_hooks_sdk.html" target="_blank">
+                    Props and Hooks
+                </TextLink>
+                section of the Ellucian Experience SDK documentation.
+            </Typography>
+        </div>
+    );
 };
 
 PropsPage.propTypes = {
