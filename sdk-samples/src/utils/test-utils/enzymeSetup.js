0fd29eb145587049540a40ab2ed1ab29cd9205ff
Ramsey Shehadeh <ramsey.shehadeh@ellucian.com>
2022-07-01 13:44:03 -0400

    Restore lint rule exception. Fix CacheCard unit test.

--- sdk-samples/src/utils/test-utils/enzymeSetup.js
+++ sdk-samples/src/utils/test-utils/enzymeSetup.js
@@ -7,6 +7,7 @@ var nodeCrypto = require('crypto');
 // mocks the standard browser crypto function in the global environment; this is necessary for
 // running SDK unit tests, as the Path component library depends on the presence of crypto
 global.crypto = {
+    // eslint-disable-next-line no-sync
     getRandomValues: function(buffer) { return nodeCrypto.randomFillSync(buffer);}
 };
 
