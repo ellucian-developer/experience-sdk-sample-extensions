212afbc6970ec2e1e4bf8c6f8bef038ebbc45e89
Tyler Nadolski <tyler.nadolski@ellucian.com>
2023-02-27 12:41:06 -0500

    EET-1700: slim down UTs

--- sdk-samples/src/test/helpers/manifest.helper.test.js
+++ sdk-samples/src/test/helpers/manifest.helper.test.js
@@ -6,34 +6,4 @@ describe('validates i18nHelper', () => {
 
         expect(enUSTranslation).toBe('Props Title');
     });
-    it.skip('validates with en-AU', () => {
-        const enAUTranslation = i18nHelper('en-AU', 'Manifest-title');
-
-        // TODO: update to use be whatever the translation becomes
-        expect(enAUTranslation).toBe('English AU Title');
-    });
-    it.skip('validates with en-GB', () => {
-        const enGBTranslation = i18nHelper('en-GB', 'Manifest-title');
-
-        // TODO: update to use be whatever the translation becomes
-        expect(enGBTranslation).toBe('English GB Title');
-    });
-    it.skip('validates with es', () => {
-        const esTranslation = i18nHelper('es', 'Manifest-title');
-
-        // TODO: update to use be whatever the translation becomes
-        expect(esTranslation).toBe('Spanish Title');
-    });
-    it.skip('validates with fr-CA', () => {
-        const frCATranslation = i18nHelper('fr-CA', 'Manifest-title');
-
-        // TODO: update to use be whatever the translation becomes
-        expect(frCATranslation).toBe('French Canadian Title');
-    });
-    it.skip('validates with ar', () => {
-        const arTranslation = i18nHelper('ar', 'Manifest-title');
-
-        // TODO: update to use be whatever the translation becomes
-        expect(arTranslation).toBe('Arabic Title');
-    });
 })
