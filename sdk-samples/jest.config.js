4f20bda810327754df1029f9d4f2b5079de7ced8
Ramsey Shehadeh <ramsey.shehadeh@ellucian.com>
2025-08-12 07:08:18 -0400

    Remove extraneous tranform ignore from jest config.

--- sdk-samples/jest.config.js
+++ sdk-samples/jest.config.js
@@ -22,10 +22,6 @@ module.exports = {
     transform: {
         '^.+\\.(js|jsx)$': '<rootDir>/node_modules/babel-jest'
     },
-    // transformIgnorePatterns: [
-    //     // '[/\\\\]node_modules[/\\\\](?!react-design-system|experience-extension|react-day-picker|date-fns).+\\.(js|jsx|mjs|cjs)$',
-    //     "node_modules",
-    // ],
     moduleNameMapper: {
         '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$': '<rootDir>/__mocks__/fileMock.js',
         '\\.(css|scss)$': 'identity-obj-proxy'
